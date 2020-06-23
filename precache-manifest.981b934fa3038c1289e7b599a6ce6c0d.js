7f465fec238752903b64a4f7c3e4bebff2bc80b1
RajonDey <rcirclebd@gmail.com>
2020-06-24 02:57:39 +0600

    Updates

---
+++ precache-manifest.981b934fa3038c1289e7b599a6ce6c0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce40548ffa366240a481b7af9fbc7c7",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "b53cad83de33456f4c03",
+    "url": "/react-todo/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7baf13a5724d367bc285",
+    "url": "/react-todo/static/css/main.c91bcf38.chunk.css"
+  },
+  {
+    "revision": "b53cad83de33456f4c03",
+    "url": "/react-todo/static/js/2.08d89e7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.08d89e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7baf13a5724d367bc285",
+    "url": "/react-todo/static/js/main.701ab9db.chunk.js"
+  },
+  {
+    "revision": "153680d245515a9047d7",
+    "url": "/react-todo/static/js/runtime-main.f10aab56.js"
+  }
+]);
